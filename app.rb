bb06828a8498c8a89fefe15a176f0a3b6289e8e7
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-01-25 04:53:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,8 +8,6 @@ class App < Sinatra::Base
 
   # Add your post route and action below
   post '/food' do
-    @name = params[:name]
-    @food = params[:favorite_food]
-    params.to_s
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
   end
 end
